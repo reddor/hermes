e856454656ecf66b6eba0a6548d007e89ffb04b7
Simon Ley <simon.ley@gmail.com>
2019-04-24 17:39:10 +0200

    delete backup folder, fix template

--- template/main.js
+++ template/main.js
@@ -4,6 +4,6 @@
 document.body.removeChild(document.body.firstChild);
 c.style.display = "none";
 
-//#include synth2.js
-// you might want to call this during an onclick-event so browser-policies don't suspend the audio context... 
+//#include synth.js
+//you might want to call this during an onclick-event so browser-policies don't suspend the audio context... 
 InitSynth();
